3777f9fd65c8ec93e5f1af8cf0d8d34b38b24e19
Cristian <crigerprogrammer@gmail.com>
2020-12-17 18:10:57 -0600

    cambios graficado

--- graficado/graficado_simple.py
+++ graficado/graficado_simple.py
@@ -1,4 +1,4 @@
-import bokeh.plotting import figure, output_file, show
+from bokeh.plotting import figure, output_file, show
 
 if __name__ == '__main__':
     output_file('graficado_simple.html')
@@ -14,4 +14,3 @@ if __name__ == '__main__':
 
     fig.line(x_vals, y_vals, line_width=2)
     show(fig)
-    
